17154dc87d37f68854ae4d3fd9473e7e005eb39d
onemonday <karakyrt13534@gmail.com>
2022-01-29 13:56:43 +0500

    .

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include <time.h>
 
 int random() {
-    unsigned int randomNumber = rand() % 4;
-    if (randomNumber == 0)
-        return 4;
-    else return randomNumber;
+    return rand() % 4;
 }
 
 class Cell {
 public:
+    Cell(unsigned int value = 0) {
+        Cell::value = value;
+    }
+
     void moveUp();
     void moveDown();
     void moveRight();
@@ -17,27 +18,51 @@ public:
 
     void printValue();
     void updateValue(unsigned int newValue);
+    static void createNewSquare(Cell (&field)[4][4]);
 private:
-    unsigned int value = 0;
+    unsigned int value;
 };
 
 int main() {
     srand(time(NULL));
 
     Cell field[4][4];
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            field[i][j].updateValue(random());
+
+    // Game start
+    Cell::createNewSquare(field);
+
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 4; j++)
             field[i][j].printValue();
-        }
         std::cout << std::endl;
     }
+
+    return 0;
 }
 
 void Cell::printValue(){
-    std::cout << value;
+    if (value == 0)
+        std::cout << "_";
+    else std::cout << value;
 }
 
 void Cell::updateValue(unsigned int newValue) {
-    value = newValue;.
+    value = newValue;
+}
+
+void Cell::createNewSquare(Cell (& field)[4][4]) {
+    while (true) {
+        unsigned int xAxis = random();
+        unsigned int yAxis = random();
+        if (field[xAxis][yAxis].value == 0)
+            continue;
+        else {
+            unsigned int newValue = random();
+            if (newValue != 0)
+                newValue = 2;
+            else newValue = 4;
+            field[xAxis][yAxis].value = newValue;
+            break;
+        }
+    }
 }
